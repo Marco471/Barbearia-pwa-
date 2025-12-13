cc437338a0bfd33ae60b684b7ea8ed5629a27d80
Marcos de Lima <marcosww.15@gmail.com>
2025-12-12 23:23:41 -0300

    Atualizando service worker antes de iniciar novo projeto

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u00u37kkgcg"
+    "revision": "0.f1p3ai7cmto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
